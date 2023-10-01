f6d780a9dd882e73b6b24b52a30a815efed23715
Yu <webdevyu@gmail.com>
2023-10-01 18:48:52 -0300

    style adjustments

--- src/components/single-country/single-country-style.js
+++ src/components/single-country/single-country-style.js
@@ -8,7 +8,7 @@ export const InfoContainer = styled.section`
   padding: 0 96px;
   gap: 64px;
 
-  min-height: 66vh;
+  min-height: 68vh;
   @media (max-width: 767px) {
     flex-direction: column;
     padding: 0;
